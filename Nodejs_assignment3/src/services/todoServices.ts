5d24b2edd3ea49261af86284654bf0edd95318a8
ShahManish2055 <1903101@ritindia.edu>
2024-01-04 12:39:43 +0545

    final commit

--- Nodejs_assignment3/src/services/todoServices.ts
+++ Nodejs_assignment3/src/services/todoServices.ts
@@ -1,45 +1,4 @@
 
-
-// const todos: Todo[] = [
-//   { id: 524431, title: "Buy groceries" },
-//   { id: 2232243, title: "Finish coding assignment" },
-// ];
-
-// export const todoService = {
-//   getTodos: (): Todo[] => todos,
-
-//   addTodo: (id: number, title: string): Todo => {
-//     const newTodo: Todo = { id, title };
-//     todos.push(newTodo);
-//     return newTodo;
-//   },
-
-//   updateTodo: (id: number, title: string): Todo | null => {
-//     const todoToUpdateIndex = todos.findIndex((todo) => todo.id === id);
-
-//     if (todoToUpdateIndex !== -1) {
-//       todos[todoToUpdateIndex] = {
-//         id,
-//         title,
-//       };
-
-//       return todos[todoToUpdateIndex];
-//     } else {
-//       return null;
-//     }
-//   },
-
-//   deleteTodo: (id: number): void => {
-//     const index = todos.findIndex((todo) => todo.id === id);
-//     if (index !== -1) {
-//       todos.splice(index, 1);
-//     }
-//   },
-// };
-
-
-
-
 import { Todo } from "../models/todoModel";
 import BaseModel from "../models/baseModel";
 
